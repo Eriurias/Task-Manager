b2af70a58034ee083f0f5f28419c8b4c65e475e9
Selim <eriurias1995@icloud.com>
2017-01-30 22:32:08 +0500

    Add files

---
+++ task_manager.cpp
@@ -0,0 +1,139 @@
+#include "precompiled.h"
+
+CTaskManager *g_pTaskManager = NULL;
+
+CTaskManager::CTaskProp::CTaskProp(edict_t * owner, task_handle_t handle, float time, float currenttime, task_types_t type, int tid, int repeat)
+{
+	m_pOwner = owner;
+	m_pHandle = handle;
+	m_flCallTime = time;
+	m_flStartTime = currenttime;
+	m_iType = type;
+	m_iID = type;
+	m_iRepeat = repeat;
+}
+
+void CTaskManager::SetTask(edict_t *owner, task_handle_t handle, float time, task_types_t type, int tid, int repeat)
+{
+	CTaskProp * pThisTask = new CTaskProp
+	(
+		owner,
+		handle,
+		time,
+		gpGlobals->time,
+		type,
+		tid,
+		repeat
+	);
+
+	m_pTaskVector.push_back(pThisTask);
+}
+
+void CTaskManager::RemoveTask(edict_t *owner, int tid)
+{
+	tid += (owner ? ENTINDEX(owner) : 0);
+
+	CTaskVectorIt it = m_pTaskVector.begin();
+	while (it != m_pTaskVector.end())
+	{
+		CTaskProp * pThisTask = (*it);
+
+		if (pThisTask->getIndex() == tid)
+		{
+			delete pThisTask;
+			m_pTaskVector.erase(it);
+			break;
+		}
+
+		it++;
+	}
+}
+
+bool CTaskManager::IsExists(edict_t *owner, int tid)
+{
+	tid += (owner ? ENTINDEX(owner) : 0);
+
+	for (auto it : m_pTaskVector)
+	{
+		if (it->getIndex() == tid)
+			return true;
+	}
+
+	return false;
+}
+
+void CTaskManager::ClearTaskByOwner(edict_t *owner)
+{
+	CTaskVectorIt it = m_pTaskVector.begin();
+	while (it != m_pTaskVector.end())
+	{
+		CTaskProp * pThisTask = (*it);
+
+		if (pThisTask->getOwner() == owner)
+		{
+			delete pThisTask;
+			m_pTaskVector.erase(it);
+			break;
+		}
+
+		it++;
+	}
+}
+
+void CTaskManager::StartFrame(float time)
+{
+	if (m_pTaskVector.empty() || m_flNextFrame > gpGlobals->time)
+	{
+		return;
+	}
+
+	m_flNextFrame = gpGlobals->time + time;
+
+	CTaskVectorIt it = m_pTaskVector.begin();
+	while (it != m_pTaskVector.end())
+	{
+		CTaskProp *pThisTask = (*it);
+
+		if (!pThisTask->isExpired())
+		{
+			it++;
+			continue;
+		}
+
+		switch (pThisTask->getType())
+		{
+		case TASK_NORMAL:
+			pThisTask->callHandle();
+
+			delete pThisTask;
+			it = m_pTaskVector.erase(it);
+
+			break;
+		case TASK_LOOP:
+			if (pThisTask->callHandle() == TASK_DESTROY)
+			{
+				delete pThisTask;
+				it = m_pTaskVector.erase(it);
+			}
+			else
+			{
+				pThisTask->updateCurrentTime();
+				it++;
+			}
+			break;
+		case TASK_REPEAT:
+			if (!pThisTask->isFinished())
+			{
+				pThisTask->callHandle();
+				pThisTask->updateCurrentTime();
+				it++;
+			}
+			else
+			{
+				delete pThisTask;
+				it = m_pTaskVector.erase(it);
+			}
+			break;
+		}
+	}
+}
---
+++ task_manager.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#define TASK_FREQUENCY 0.1f
+
+typedef enum
+{
+	TASK_NORMAL = 0x0,
+	TASK_LOOP,
+	TASK_REPEAT
+} task_types_t;
+
+typedef enum
+{
+	TASK_DESTROY = 0x0,
+	TASK_IGNORED
+} task_retvalue_t;
+
+typedef int(*task_handle_t)(edict_t *, int);
+
+class CTaskManager
+{
+private:
+	
+	class CTaskProp
+	{
+	private:
+		float m_flStartTime;
+		float m_flCallTime;
+
+		edict_t *m_pOwner;
+		task_handle_t m_pHandle;
+		task_types_t m_iType;
+
+		int m_iID;
+		int m_iRepeat;
+	public:
+		CTaskProp(void) { };
+		CTaskProp(edict_t * owner, task_handle_t handle, float time, float currenttime, task_types_t type = TASK_NORMAL, int tid = 0, int repeat = 0);
+
+		void updateCurrentTime(void)
+		{
+			m_flStartTime = gpGlobals->time;
+		}
+
+		bool isExpired()
+		{
+			return gpGlobals->time >= (m_flStartTime + m_flCallTime);
+		}
+
+		bool isFinished()
+		{
+			return (m_iRepeat < 1);
+		}
+
+		int getIndex()
+		{
+			return m_iID;
+		}
+
+		edict_t * getOwner()
+		{
+			return m_pOwner;
+		}
+
+		task_types_t getType()
+		{
+			return m_iType;
+		}
+
+		int callHandle()
+		{
+			return m_pHandle(m_pOwner, m_iType == TASK_REPEAT ? (m_iRepeat--) : 0);
+		}
+	};
+
+	int m_iTaskCount;
+	float m_flNextFrame;
+	typedef std::vector<CTaskProp *> CTaskVector;
+	typedef std::vector<CTaskProp *>::iterator CTaskVectorIt;
+	CTaskVector m_pTaskVector;
+public:
+	void SetTask(edict_t *owner, task_handle_t handle, float time, task_types_t type = TASK_NORMAL, int tid = 0, int repeat = 0);
+	void RemoveTask(edict_t *owner, int tid = 0);
+	void ClearTaskByOwner(edict_t *owner = NULL);
+	bool IsExists(edict_t *owner, int tid = 0);
+	void StartFrame(float time);
+};
+
+extern CTaskManager *g_pTaskManager;
